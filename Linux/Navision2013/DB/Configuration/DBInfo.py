a1374bff98ecc56cad3b07bb4fdcce42eca222d5
sachu1994 <107610698+sachu1994@users.noreply.github.com>
2022-11-07 14:18:31 +0530

    Delete Linux/Navision2013 directory

--- Linux/Navision2013/DB/Configuration/DBInfo.py
+++
@@ -1,6 +0,0 @@
-class PostgresDbInfo:
-    Configurl = "jdbc:postgresql://192.10.15.134/Configurator_Linux"
-    logsDbUrl = "jdbc:postgresql://192.10.15.134/Logs_new"
-    url = "jdbc:postgresql://192.10.15.134/"
-    metadataDB = "jdbc:postgresql://192.10.15.134/.metadata"
-    props = {"user":"postgres", "password":"admin", "driver": "org.postgresql.Driver"}
